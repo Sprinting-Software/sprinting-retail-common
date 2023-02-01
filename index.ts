f88d60459443706e129b485fcf108cd50518f11c
nairiabgaryan <nairi@global.id>
2023-02-01 11:03:27 +0400

    feat: Error factory for the logger service

--- index.ts
+++ index.ts
@@ -1,2 +1,3 @@
 export * from "./logger/logger.service";
 export * from "./logger/errors";
+export * from "./logger/errorFactory";
--- logger/errors.ts
+++ logger/errors.ts
@@ -28,44 +28,48 @@ export class HttpException extends DefaultHttpException implements IException {
       ')' +
       (this.contextData ? ' - errorData: ' + util.inspect(this.contextData) : '') +
       (this.message ? ' - ' + this.message : '') +
-      (this.innerError ? '\n    |-> innerError: ' + error2string(this.innerError) : '')
+      (this.innerError ? '\n    |-> innerError: ' + this.error2string(this.innerError) : '')
     );
   }
   
   toJson(): any {
     return {
       errorName: this.name,
-      innerError: error2string(this.innerError),
+      innerError: this.error2string(this.innerError),
       errorData: this.contextData,
       message: this.message,
       httpStatus: this.statusCode,
     };
   }
-}
-
-export function innerError(name, message: string, data?: Record<string, any>, innerError?: any) {
-    return createError(HttpStatus.INTERNAL_SERVER_ERROR, name,  message, data, innerError)
-}
-
-
-export function createError(status: number, name: string, message: string, data?: Record<string, any>, innerError?: any) {
-  return new HttpException(status, name, message, data, innerError)
-}
-
-function error2string(e?: any): string | undefined {
-  if (e === undefined) return undefined;
-  if (e instanceof HttpException) {
-    return e.toString();
-  } else if (e as IException) {
-    const e2 = e as IException;
-    return JSON.stringify({
-      httpStatus: e2.statusCode,
-      errorName: e2.name,
-      errorData: e2.contextData,
-      detailedMessage: e2.message,
-      innerError: error2string(e2.innerError),
-    });
-  } else {
-    return '' + e;
+  
+  private error2string(e?: any): string | undefined {
+    if (e === undefined) return undefined;
+    if (e instanceof HttpException) {
+      return e.toString();
+    } else if (e as IException) {
+      const e2 = e as IException;
+      return JSON.stringify({
+        httpStatus: e2.statusCode,
+        errorName: e2.name,
+        errorData: e2.contextData,
+        detailedMessage: e2.message,
+        innerError: this.error2string(e2.innerError),
+      });
+    } else {
+      return '' + e;
+    }
   }
 }
+
+
+export class InternalServerError extends HttpException {
+  constructor(
+    name: string,
+    message: string,
+    data?: Record<string, any>,
+    innerError?: any
+  ) {
+    super(HttpStatus.INTERNAL_SERVER_ERROR, name, message, data, innerError)
+  }
+}
+
