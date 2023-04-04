6d6906513ac982bf42456128846be12ddd284aa6
Nikola Schou <nikolaschou@gmail.com>
2023-04-04 04:42:26 +0200

    Externalize ErrorParser

---
+++ src/errorHandling/spec/ErrorParser.spec.ts
@@ -0,0 +1,36 @@
+import { AppException } from "../AppException"
+import { HttpStatus } from "@nestjs/common"
+import { ClientException } from "../ClientException"
+import { ErrorParser } from "../ErrorParser"
+import { ServerException } from "../ServerException"
+
+describe("ErrorParser", () => {
+  it("should parse ClientException", () => {
+    const e = new ClientException("ERROR_NAME", "ERROR_DESCRIPTION", { key: "value" })
+    const e2 = ErrorParser.parse(e)
+    expect(e2).toBe(e)
+    expect(e2).toEqual(e)
+    expect(e2 === e).toBeTruthy()
+  })
+  it("should parse AppException", () => {
+    const e = new AppException(HttpStatus.INTERNAL_SERVER_ERROR, "ERROR_NAME", "ERROR_DESCRIPTION", { key: "value" })
+    const e2 = ErrorParser.parse(e)
+    expect(e2).toBe(e)
+    expect(e2).toEqual(e)
+    expect(e2 === e).toBeTruthy()
+  })
+  it("should parse ClientException", () => {
+    const e = new ServerException("ERROR_NAME", "ERROR_DESCRIPTION", { key: "value" })
+    const e2 = ErrorParser.parse(e)
+    expect(e2).toBe(e)
+    expect(e2).toEqual(e)
+    expect(e2 === e).toBeTruthy()
+  })
+
+  it("should parse Error", () => {
+    const e = new Error("ERROR_NAME")
+    const e2 = ErrorParser.parse(e)
+    expect(e2).toBeInstanceOf(AppException)
+    expect(e2.errorName).toEqual("ERROR_NAME")
+  })
+})
--- src/filter/GlobalErrorFilter.ts
+++ src/filter/GlobalErrorFilter.ts
@@ -3,6 +3,7 @@ import { LoggerService } from "../logger/LoggerService"
 import { AppException } from "../errorHandling/AppException"
 import { CustomBadRequestException } from "../errorHandling/CustomBadRequestException"
 import { LogContext } from "../common/LogContext"
+import { ErrorParser } from "../errorHandling/ErrorParser"
 
 @Injectable()
 @Catch()
@@ -10,28 +11,12 @@ export class GlobalErrorFilter implements ExceptionFilter {
   constructor(private readonly logger: LoggerService, private readonly logContext: LogContext) {}
 
   catch(error: Error, host: ArgumentsHost): void {
-    const exception = this.parseError(error)
+    const exception = ErrorParser.parse(error)
     this.logger.logError(exception, this.logContext)
 
     const ctx = host.switchToHttp()
     const response = ctx.getResponse()
 
     response.status(exception.getStatus()).send(exception.getResponse())
-  }
-
-  private parseError(error: Error): AppException {
-    if (error instanceof AppException) {
-      return error
-    }
-
-    if (error.name === "BadRequestException") {
-      return new CustomBadRequestException(<BadRequestException>error)
-    }
-
-    if ("getStatus" in error && typeof error.getStatus === "function") {
-      return new AppException(error.getStatus(), error.name, error.message).setInnerError(error)
-    }
-
-    return new AppException(500, error.name, error.message, error)
   }
 }
