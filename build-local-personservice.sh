2686f95716c6e0a7eaba6423ed7c138dc0c4ad93
Nikola Schou <nikolaschou@gmail.com>
2023-04-08 08:48:33 +0200

    Adding a build-file to allow local development for personservice

---
+++ build-local-personservice.sh
@@ -0,0 +1,1 @@
+ tsc --emitDecoratorMetadata && npm pack && cd ../sprinting_personservice/PersonServiceApi && npm install ../../sprinting-retail-common/sprinting-retail-common-1.2.1.tgz && cd ../../sprinting-retail-common
