23998b076e8679139a9f1ba6ce85085f60f353d1
Goran Blazin <goran.blazin@gmail.com>
2024-04-11 12:29:55 +0200

    Introduced PrincipalEnum baseData

---
+++ src/baseData/PrincipalEnum.ts
@@ -0,0 +1,13 @@
+// WHEN ADDING VALUES HERE, PLEASE GET IT CONFIRMED WITH NIKOLA
+export enum PrincipalEnum {
+  PersonServiceApi = 'PersonServiceApi',
+  IDP = 'IDP',
+  RecommendationBE = 'RecommendationBE',
+  LoyaltyBE = 'LoyaltyBE',
+  TenantDataServiceBE = 'TenantDataServiceBE',
+  BifrostBackend = 'BifrostBackend',
+  RGS = 'RGS',
+  PlatformConfigBE = 'PlatformConfigBE',
+  GlobalBackOffice = 'GlobalBackOffice',
+  All = 'All',
+}
