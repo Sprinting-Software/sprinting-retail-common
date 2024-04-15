2af82c3432560cf9131606b254177876fc005c9f
Goran Blazin <goran.blazin@gmail.com>
2024-04-15 13:56:26 +0200

    Added check for http server when initiating CommonAppModule. Increased version to v6.0.8

--- src/helpers/LoadBalancingTimeoutBootstrap.ts
+++ src/helpers/LoadBalancingTimeoutBootstrap.ts
@@ -5,13 +5,15 @@ export class LoadBalancingTimeoutBootstrap implements OnApplicationBootstrap {
   constructor(private readonly refHost: any, private readonly logger: LoggerService) {}
 
   onApplicationBootstrap() {
-    const server = this.refHost.httpAdapter.getHttpServer()
-    server.keepAliveTimeout = 65000 // Ensure all inactive connections are terminated by the ALB, by setting this a few seconds higher than the ALB idle timeout
-    server.headersTimeout = 66000 // Ensure the headersTimeout is set higher than the keepAliveTimeout due to this nodejs regression bug: https://github.com/nodejs/node/issues/27363
-    // eslint-disable-next-line
-    this.logger.info(
-      __filename,
-      `Running LoadBalancingTimeoutBootstrap to fix issues with the load balancers such as ALB ${server}`
-    )
+    const httpServer = this.refHost.httpAdapter?.getHttpServer()
+    if (httpServer) {
+      httpServer.keepAliveTimeout = 65000 // Ensure all inactive connections are terminated by the ALB, by setting this a few seconds higher than the ALB idle timeout
+      httpServer.headersTimeout = 66000 // Ensure the headersTimeout is set higher than the keepAliveTimeout due to this nodejs regression bug: https://github.com/nodejs/node/issues/27363
+      // eslint-disable-next-line
+      this.logger.info(
+        __filename,
+        `Running LoadBalancingTimeoutBootstrap to fix issues with the load balancers such as ALB ${httpServer}`
+      )
+    }
   }
 }
