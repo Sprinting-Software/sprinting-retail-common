bbf5dbdeb804f9107a050e82fb6bb543a11a8d8e
Nikola Schou <ns@sprintingsoftware.com>
2024-11-23 18:34:30 +0200

    Eslint

--- src/logger/RawLogger.ts
+++ src/logger/RawLogger.ts
@@ -1,6 +1,7 @@
 export const RawLogger = {
   debug: (...args: Array<any>) => {
     if (process.env.DEBUG_ERROR_HANDLING) {
+      // eslint-disable-next-line no-console
       console.log(...args)
     }
   },
