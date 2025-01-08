0ccbeaa5f9b1d159b12fedc1025cf4d51a8b1a21
Nikola Schou <ns@sprintingsoftware.com>
2025-01-08 14:21:45 +0100

    Improved error handling

--- src/logger/spec/ElkRestApi.cross.spec.ts
+++ src/logger/spec/ElkRestApi.cross.spec.ts
@@ -1,14 +1,17 @@
 import { ElkRestApi } from "../ElkRestApi"
 import { TestUtil } from "./TestUtil"
 
-describe("ElkRestApi integration test", () => {
+/**
+ * This should not be run in the pipeline as it is an integration test
+ */
+describe.skip("ElkRestApi integration test", () => {
   let elkRestApi: ElkRestApi
 
   beforeEach(() => {
     // API key nonp-ecobe-tcplogs
     const apiKey = "xxx"
     // Mock ElkRestApi
-    elkRestApi = new ElkRestApi({ apiKey, endpoint: "http://10.0.0.170:9200", indexName: "a-ecobe-event-2025.02" })
+    elkRestApi = new ElkRestApi({ apiKey, endpoint: "yyy", indexName: "zzz" })
   })
 
   test("should process logs and send them to ElkRestApi", async () => {
